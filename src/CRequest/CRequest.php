e620d395456f73ed1d69252461508e64f9e9caad
Bo Christer Eriksson <bochrister.1.4@telia.com>
2013-11-29 15:20:54 +0100

    trial(v0.2.05)

--- src/CRequest/CRequest.php
+++ src/CRequest/CRequest.php
@@ -65,49 +65,59 @@ Sisti metoden Init($baseUrl = null) sparas alla värden som tagits fram i variabl
 */
 
 
-	public function Init($baseUrl = null) {
-
-	$requestUri = $_SERVER['REQUEST_URI'];
-	$scriptPart = $scriptName = $_SERVER['SCRIPT_NAME'];    
-
-
-     	if(substr_compare($requestUri, $scriptName, 0, strlen($scriptName))) {
-     	$scriptPart = dirname($scriptName);
-     	}
-
-	$query = trim(substr($requestUri, strlen(rtrim($scriptPart, '/'))), '/');  
-	$pos = strcspn($query, '?');
-
-    	if($pos) {
-    	$query = substr($query, 0, $pos);
-    	}
-
-    	if(substr($query, 0, 1) === '?' && isset($_GET['q'])) {
-    	$query = trim($_GET['q']);
-    	}
-
-	$splits = explode('/', $query);
+ /**
+* Parse the current url request and divide it in controller, method and arguments.
+*
+* Calculates the base_url of the installation. Stores all useful details in $this.
+*
+* @param $baseUrl string use this as a hardcoded baseurl.
+*/
+  public function Init($baseUrl = null) {
+    $requestUri = $_SERVER['REQUEST_URI'];
+    $scriptName = $_SERVER['SCRIPT_NAME'];
     
-    	$controller =  !empty($splits[0]) ? $splits[0] : 'index';
-    	$method     =  !empty($splits[1]) ? $splits[1] : 'index';
-    	$arguments = $splits;
-    	unset($arguments[0], $arguments[1]); // remove controller & method part from argument list
-
-    	$currentUrl = $this->GetCurrentUrl();
-       $parts       = parse_url($currentUrl);
-    	$baseUrl     = !empty($baseUrl) ? $baseUrl : "{$parts['scheme']}://{$parts['host']}" . (isset($parts['port']) ? ":{$parts['port']}" : '') . rtrim(dirname($scriptName), '/');
-
-
-    	$this->base_url     = rtrim($baseUrl, '/') . '/';
-    	$this->current_url  = $currentUrl;
-    	$this->request_uri  = $requestUri;
-    	$this->script_name  = $scriptName;
-    	$this->query        = $query;
-    	$this->splits        = $splits;
-    	$this->controller    = $controller;
-    	$this->method        = $method;
-    	$this->arguments    = $arguments;
-  	}
+    // Compare REQUEST_URI and SCRIPT_NAME as long they match, leave the rest as current request.
+    $i=0;
+    $len = min(strlen($requestUri), strlen($scriptName));
+    while($i<$len && $requestUri[$i] == $scriptName[$i]) {
+      $i++;
+    }
+    $request = trim(substr($requestUri, $i), '/');
+  
+    // Remove the ?-part from the query when analysing controller/metod/arg1/arg2
+    $queryPos = strpos($request, '?');
+    if($queryPos !== false) {
+      $request = substr($request, 0, $queryPos);
+    }
+    
+    // Check if request is empty and querystring link is set
+    if(empty($request) && isset($_GET['q'])) {
+      $request = trim($_GET['q']);
+    }
+    $splits = explode('/', $request);
+    
+    // Set controller, method and arguments
+    $controller = !empty($splits[0]) ? $splits[0] : 'index';
+    $method                 = !empty($splits[1]) ? $splits[1] : 'index';
+    $arguments = $splits;
+    unset($arguments[0], $arguments[1]); // remove controller & method part from argument list
+    
+    // Prepare to create current_url and base_url
+    $currentUrl = $this->GetCurrentUrl();
+    $parts          = parse_url($currentUrl);
+    $baseUrl                 = !empty($baseUrl) ? $baseUrl : "{$parts['scheme']}://{$parts['host']}" . (isset($parts['port']) ? ":{$parts['port']}" : '') . rtrim(dirname($scriptName), '/');
+    
+    // Store it
+    $this->base_url          = rtrim($baseUrl, '/') . '/';
+    $this->current_url = $currentUrl;
+    $this->request_uri = $requestUri;
+    $this->script_name = $scriptName;
+    $this->request = $request;
+    $this->splits         = $splits;
+    $this->controller         = $controller;
+    $this->method         = $method;
+    $this->arguments = $arguments;
+  }
 
 /*Metod function GetCurrentUrl tar sedan fram den fullständiga adressen (inte bara den lokala)
 till efterfrågad sida genom anropa metoden med hjälp av skrivna data för serverns port och variabler av typ
