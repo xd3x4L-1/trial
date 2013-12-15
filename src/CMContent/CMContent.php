9dc952a99a0c85e230839a733f17245a76d092ea
Bo Christer Eriksson <bochrister.1.4@telia.com>
2013-12-15 21:35:33 +0100

    trial(v0.2.15)

--- src/CMContent/CMContent.php
+++ src/CMContent/CMContent.php
@@ -158,7 +158,7 @@ class CMContent extends CObject implements IHasSQL, ArrayAccess {
 case 'php': $data = nl2br(makeClickable(eval('?>'.$data))); break;
 case 'html': $data = nl2br(makeClickable($data)); break;*/
 
-
+	  case 'htmlpurify': $data = nl2br(CHTMLPurifier::Purify($data)); break;
 	  case 'bbcode': $data = nl2br(bbcode2html(htmlEnt($data))); break;
       case 'plain':
       default: $data = nl2br(makeClickable(htmlEnt($data))); break;
